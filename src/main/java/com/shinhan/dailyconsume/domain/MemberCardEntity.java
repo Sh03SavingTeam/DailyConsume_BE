7fd1eeb2754cf2491f1f36f8a71e11bfc13fee43
BaekInHyeok <bih63879@gmail.com>
2024-08-13 12:34:52 +0900

    MemberCardEntity 연관 관계 설정

--- src/main/java/com/shinhan/dailyconsume/domain/MemberCardEntity.java
+++ src/main/java/com/shinhan/dailyconsume/domain/MemberCardEntity.java
@@ -3,9 +3,13 @@ package com.shinhan.dailyconsume.domain;
 import java.sql.Timestamp;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,4 +27,11 @@ public class MemberCardEntity {
 	String cardNum;
 	Timestamp expirationDate;
 	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="cardId")
+	private CardEntity card;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="memberId")
+	private MemberEntity member;
 }
