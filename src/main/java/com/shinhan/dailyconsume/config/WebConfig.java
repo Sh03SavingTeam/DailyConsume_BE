c4b146befb50c268197faa0cbedafc427efcafb7
sinsang2752 <sinsang2752@naver.com>
2024-08-21 09:52:57 +0900

    commit: merge 전 수정

--- src/main/java/com/shinhan/dailyconsume/config/WebConfig.java
+++ src/main/java/com/shinhan/dailyconsume/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Webconfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer{
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
