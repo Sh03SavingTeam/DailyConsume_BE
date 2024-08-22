bfef46f7bc8b63da3533f57394d9d1eb3549dd7e
aaa67 <enen9032@naver.com>
2024-08-22 19:35:45 +0900

    [Feat] 포인트 환급 기능 구현 완료 2024-08-22 19:35

--- src/main/java/com/shinhan/dailyconsume/controller/DiscountingInfoController.java
+++ src/main/java/com/shinhan/dailyconsume/controller/DiscountingInfoController.java
@@ -1,10 +1,12 @@
 package com.shinhan.dailyconsume.controller;
 
+import com.shinhan.dailyconsume.dto.DiscountingDTO;
 import com.shinhan.dailyconsume.dto.DiscountingInfoDTO;
 import com.shinhan.dailyconsume.service.DiscountingInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +17,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class DiscountingInfoController {
 
     final DiscountingInfoService dService;
 
-    @GetMapping("/discountinginfo/{memberId}")
+    @GetMapping("/mypage/discountinginfo/{memberId}")
     public ResponseEntity<Object> getDiscountingInfos(@PageableDefault(size=5) Pageable pageable, @PathVariable("memberId") String memberId){
-        return ResponseEntity.ok(dService.getDiscountingInfos(pageable, memberId));
+        DiscountingDTO discountingDTO = dService.getDiscountingInfos(pageable, memberId);
+//        if(discountingDTO==null){
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//        }
+        return ResponseEntity.ok(discountingDTO);
     }
 }
--- src/main/java/com/shinhan/dailyconsume/service/DiscountingInfoServiceImpl.java
+++ src/main/java/com/shinhan/dailyconsume/service/DiscountingInfoServiceImpl.java
@@ -1,10 +1,14 @@
 package com.shinhan.dailyconsume.service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.shinhan.dailyconsume.domain.ConsumeCategoryEntity;
 import com.shinhan.dailyconsume.dto.DiscountingDTO;
-import com.shinhan.dailyconsume.repository.PayHistoryRepository;
+import com.shinhan.dailyconsume.dto.PayHistoryDTO;
+import com.shinhan.dailyconsume.repository.ConsumeCategoryRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -18,22 +22,49 @@ import lombok.RequiredArgsConstructor;
 public class DiscountingInfoServiceImpl implements DiscountingInfoService {
 	
 	final DiscountingInfoRepository dRepo;
-	private final PayHistoryRepository pRepo;
+	private final ConsumeCategoryRepository cRepo;
+	private final PayHistoryService pService;
 
 	//할인 정보 조회
 	@Override
 	public DiscountingDTO getDiscountingInfos(Pageable pageable, String memberId) {
 
 		//가장 많은 지출이 있는 카테고리 구하기
-		List<Object[]> result = pRepo.findCategoryPayByMember(memberId);
+		List<PayHistoryDTO> payHistories = pService.getPayHistory(memberId);
 
-		if (!result.isEmpty()) {
+		if (!payHistories.isEmpty()) {
 
-			String category = (String) result.get(0)[0];
-			int categoryId = (Integer) result.get(0)[1];
+			payHistories.sort(Comparator.comparing(PayHistoryDTO::getPayAmount).reversed());
+
+			System.out.println(payHistories.toString());
+
+			String category = payHistories.get(0).getConsumeCategory();
+
+			ConsumeCategoryEntity consumeCategory = cRepo.findByConsumeName(category);
 
 			//카테고리별 맞춤 할인 정보 구하기
-			//return
+			List<DiscountingInfoDTO> discountingDTOS = dRepo.findByConsumeCategory(consumeCategory).stream()
+					.map(discountingInfoEntity -> new DiscountingInfoDTO(discountingInfoEntity))
+					.collect(Collectors.toList());
+
+			//할인 정보 총 개수
+			int totalCount = discountingDTOS.size();
+
+			List<DiscountingInfoDTO> pagingInfos = new ArrayList<>();
+
+			int startIndex = (int) pageable.getOffset();
+			int endIndex = Math.min(startIndex + pageable.getPageSize(), totalCount);
+
+			List<DiscountingInfoDTO> paginatedInfos = new ArrayList<>(discountingDTOS).subList(startIndex, endIndex);
+
+			for(DiscountingInfoDTO discountingInfoDTO : paginatedInfos){
+				pagingInfos.add(discountingInfoDTO);
+			}
+
+			DiscountingDTO discountingDTO = new DiscountingDTO(category, totalCount, pagingInfos);
+
+			return discountingDTO;
+
 		}
 
 		return null;
