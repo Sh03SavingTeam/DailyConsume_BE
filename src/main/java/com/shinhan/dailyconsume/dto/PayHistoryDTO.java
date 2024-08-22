77d7c471aa70eee4c06687f4fe44cc96b74bfb66
sangho5550 <baeksh0118@gmail.com>
2024-08-22 16:27:07 +0900

    캘린더구현

--- src/main/java/com/shinhan/dailyconsume/dto/PayHistoryDTO.java
+++ src/main/java/com/shinhan/dailyconsume/dto/PayHistoryDTO.java
@@ -39,17 +39,6 @@ public class PayHistoryDTO {
 		this.consumeCategory = consumeCategory;
 	}
 	
-	  private Long payId;          // 결제내역 ID
-    private Long payAmount;      // 결제 금액
-    private Timestamp payDate;   // 결제 일자
-    
-    private Long consumeCategoryId;  // 소비 카테고리 ID
-    private String consumeCategoryName; // 소비 카테고리 이름 (옵션)
-    
-    private String cardNum;      // 카드 번호
-    private String cardName;     // 카드 이름 (옵션)
-    
-    private String strRegNum;  // 사업자 등록번호
-    private String storeName;    // 상호명 (옵션)
+	  
     
 }
